6d13023a99ea9fd8ec6ec4b0bd21725332af6d15
minkyeongJ <jomk2221@naver.com>
2023-04-12 03:21:46 +0900

    Feat: 홈 디자인 수정 및 todo 이동 버튼 추가

--- src/components/view/home/Home.tsx
+++ src/components/view/home/Home.tsx
@@ -1,7 +1,29 @@
+import routerConst from "../../../helper/routerConst";
+
 const Home = () => {
   return (
     <>
-      <main>Home</main>
+      <main>
+        <section>
+          <iframe
+            src="https://giphy.com/embed/KCjV5CBW128LzsgH1Y"
+            width="400"
+            height="400"
+            className="my-0 mx-auto pointer-events-none"
+            allowFullScreen
+          ></iframe>
+        </section>
+        <section className="mt-10">
+          <div className="max-w-full text-center">
+            <a
+              href={routerConst.TODO}
+              className="bg-transparent hover:bg-pink-500 text-violet-700 font-semibold hover:text-white py-2 px-4 border border-violet-500 hover:border-transparent rounded mx-auto"
+            >
+              GO TODO
+            </a>
+          </div>
+        </section>
+      </main>
     </>
   );
 };
